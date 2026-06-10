33da6c5371c64fa5d978b75b448073d03052cf47
yunzhijunxi123 <339754794@qq.com>
2026-06-10 21:59:18 +0800

    进一步改进

--- hrbust_static/js/index.js
+++ hrbust_static/js/index.js
@@ -1,16 +1,3 @@
-// const computerScienceBeautifulPic = document.getElementById(
-//   "computer-science-beautiful-pic",
-// );
-// const schoolOfficialAccountPic = document.getElementById(
-//   "school-official-account-pic",
-// );
-// computerScienceBeautifulPic.addEventListener("hover", function (event) {
-//   alert("123");
-// });
-// schoolOfficialAccountPic.addEventListener("hover", function (event) {
-//   alert("123");
-// });
-
 /*自动切换图片*/
 //利用闭包 只有toggleChange能访问timer
 //创建匿名函数 让函数作为返回值返回 定义变量引用他
@@ -32,7 +19,7 @@ const toggleChange = (function () {
 })();
 
 toggleChange(); //默认进入
-const outer = document.getElementsByClassName("outer")[0];
+const outer = document.getElementsByClassName("banner")[0];
 outer.onmouseenter = () => {
   //鼠标进入则关闭
   toggleChange();
@@ -46,8 +33,8 @@ outer.onmouseleave = () => {
 //     toggleChange();
 //   };
 
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
+const prev = document.getElementById("bannerPrev");
+const next = document.getElementById("bannerNext");
 
 prev.onclick = () => {
   changeImg("prev");
@@ -59,23 +46,22 @@ next.onclick = () => {
 /*
             changeImg 用来切换图片
                 dir 切换图片的方向  next   prev
-        */
+*/
 function changeImg(dir) {
-  const current = document.querySelector(".img-list .current");
+  const current = document.querySelector(".banner .current");
   //获取下一张图片
   let next;
   if (dir === "next") {
     next =
       current.nextElementSibling ||
-      document.querySelector(".img-list li:first-child");
+      document.querySelector(".banner li:first-child");
     // current 的下一个兄弟元素，如果已经是最后一个，就返回第一个元素
   } else if (dir === "prev") {
     next =
       current.previousElementSibling ||
-      document.querySelector(".img-list li:last-child");
+      document.querySelector(".banner li:last-child");
     //下一个或最后一个
   }
   current.classList.remove("current");
   next.classList.add("current");
-  changeDot(dir);
 }
