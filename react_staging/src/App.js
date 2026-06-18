fcaf977f7fc61ea39c2ade1632f7c6061fc153a3
yunzhijunxi123 <339754794@qq.com>
2026-06-18 23:13:53 +0800

    react脚手架基本介绍

---
+++ react_staging/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+//将 App 组件作为默认导出，供其他文件导入使用。
+export default App;
