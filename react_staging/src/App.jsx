b24c5044d92ed5042c260d36f425c2c93d41334c
yunzhijunxi123 <339754794@qq.com>
2026-06-21 15:13:08 +0800

    axios基础

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,63 +1,9 @@
-﻿import React, { Component } from "react";
-import Header from "./components/Header";
-import List from "./components/List";
-import Footer from "./components/Footer";
-import "./App.css";
+﻿import React, { Component } from 'react'
+
 export default class App extends Component {
-  state = {
-    todos: [
-      { id: "001", name: "吃饭", done: true },
-      { id: "002", name: "睡觉", done: true },
-      { id: "003", name: "打代码", done: false },
-      { id: "004", name: "购物", done: false },
-    ],
-  };
-  addTodo = (todoObj) => {
-    const { todos } = this.state;
-    const newTodos = [todoObj, ...todos];
-    this.setState({ todos: newTodos });
-  };
-  updateTodo = (id, done) => {
-    const { todos } = this.state;
-    const newTodos = todos.map((todoObj) => {
-      if (todoObj.id === id) return { ...todoObj, done };
-      else return todoObj;
-    });
-    this.setState({ todos: newTodos });
-  };
-  deleteTodo = (id) => {
-    const { todos } = this.state;
-    const newTodos = todos.filter((todoObj) => {
-      return todoObj.id !== id;
-    });
-    this.setState({ todos: newTodos });
-  };
-  // 全选 / 取消全选切换
-  checkAllToDo = (done) => {
-    const { todos } = this.state;
-    const newTodos = todos.map((todoObj) => {
-      return { ...todoObj, done };
-    });
-    this.setState({ todos: newTodos });
-  };
-  // 清除已完成的todo
-  clearAllDone = () => {
-    const { todos } = this.state;
-    const newTodos = todos.filter((todoObj) => {
-      return !todoObj.done;//留下没做的
-    });
-    this.setState({ todos: newTodos });
-  };
   render() {
-    const { todos } = this.state;
     return (
-      <div className="todo-container">
-        <div className="todo-wrap">
-          <Header addTodo={this.addTodo} />
-          <List todos={todos} updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} />
-          <Footer todos={todos} checkAllToDo={this.checkAllToDo} clearAllDone={this.clearAllDone} />
-        </div>
-      </div>
-    );
+      <div>App</div>
+    )
   }
 }
