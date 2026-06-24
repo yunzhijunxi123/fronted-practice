337e58d7beea4a941a04b6098132fd6a3ee55e5e
yunzhijunxi123 <339754794@qq.com>
2026-06-24 22:14:57 +0800

    antd按需引入 自定主题

--- react_staging/config-overrides.js
+++ react_staging/config-overrides.js
@@ -1,3 +1,16 @@
-﻿const { override } = require('customize-cra');
+﻿//配置具体的修改规则
+const { override, fixBabelImports,addLessLoader} = require('customize-cra');
 
-module.exports = override();
+module.exports = override(
+  fixBabelImports('import', {//按需引入
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+	}),
+	addLessLoader({//自定义主题
+		lessOptions:{
+			javascriptEnabled: true,
+			modifyVars: { '@primary-color': 'green' },
+		}
+	}),
+);
--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -1,23 +1,30 @@
 ﻿import React, { Component } from 'react'
-import { Button,DatePicker} from 'antd'
-import {SearchOutlined, WechatOutlined,WeiboOutlined} from "@ant-design/icons"
+import { Button, DatePicker, ConfigProvider } from 'antd'
+import { SearchOutlined, WechatOutlined, WeiboOutlined } from '@ant-design/icons'
 
 export default class App extends Component {
-  
+
   render() {
-    //antd element vant
     return (
-      <div>
-        <Button type="primary">Primary Button</Button>
-        <Button>Primary Button</Button>
-        <Button type="link">Primary Button</Button>
-        <Button type="primary" icon={<SearchOutlined/>}>
-          Search
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: 'orange',
+          },
+        }}
+      >
+        <div>
+          <Button type='primary'>Primary Button</Button>
+          <Button>Default Button</Button>
+          <Button type='link'>Link Button</Button>
+          <Button type='primary' icon={<SearchOutlined />}>
+            Search
           </Button>
-        <WechatOutlined />
-        <WeiboOutlined />
-        <DatePicker />
-      </div>
+          <WechatOutlined />
+          <WeiboOutlined />
+          <DatePicker />
+        </div>
+      </ConfigProvider>
     )
   }
 }
